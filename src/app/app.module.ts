d556829e45fdb6932cf69f868230613efeee493d
kevin <kevinfiorentino.1294@gmail.com>
2020-09-09 23:18:03 -0300

    Enrutador

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
+import { Routes, RouterModule } from '@angular/router';
+
 import { AppComponent } from './app.component';
+
+import { NavbarComponent } from './components/navbar/navbar.component';
 import { ProductosComponent } from './components/productos/productos.component';
 import { TemplateProductoComponent } from './components/template-producto/template-producto.component';
+import { FavoritosComponent } from './components/favoritos/favoritos.component';
+
+
+const routes: Routes = [
+	{ path: '', redirectTo: '/productos', pathMatch: 'full' },
+	{ path: 'productos', component: ProductosComponent },
+	{ path: 'favoritos', component: FavoritosComponent },
+];
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ProductosComponent,
-    TemplateProductoComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		ProductosComponent,
+		TemplateProductoComponent,
+		NavbarComponent,
+		FavoritosComponent
+	],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		RouterModule.forRoot(routes)
+	],
+	exports: [RouterModule],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
